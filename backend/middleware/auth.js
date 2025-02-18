36693a39c82e860f0fd5a63cc6c941dda8e1863d
ItayR626 <128970368+ItayR626@users.noreply.github.com>
2025-02-18 12:59:40 +0200

    middleware auth added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,12 @@
+function isLoggedIn(req, res, next) {
+      if (req.session && req.session.user) {
+            next();
+      } else {
+            res.status(401).json({ message: "Unauthorized" });
+      }
+}
+
+module.exports = { isLoggedIn };
+      }
+      }
+}
