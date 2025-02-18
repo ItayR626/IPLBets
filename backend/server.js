40ea7a4105e60f880bd2ad0ed6b7d1ee413aa253
ItayR626 <128970368+ItayR626@users.noreply.github.com>
2025-02-18 12:38:52 +0200

    reformat

--- backend/server.js
+++ backend/server.js
@@ -31,80 +31,79 @@ const notificationRoute = require("./routes/notificationRoute");
 // Session store options
 const options = {
   host: "localhost",
-    user: "root",
-      password: "",
-        database: "project",
-        };
+  user: "root",
+  password: "",
+  database: "project",
+};
 
-        // Session store
-        const sessionStore = new MySQLStore(options);
+// Session store
+const sessionStore = new MySQLStore(options);
 
-        function generateSecretKey() {
-          return crypto.randomBytes(64).toString("hex");
-          }
+function generateSecretKey() {
+  return crypto.randomBytes(64).toString("hex");
+}
 
-          const secretKey = generateSecretKey();
+const secretKey = generateSecretKey();
 
-          console.log("Secret Key:", secretKey);
-          // Session middleware configuration
-          app.use(
-            session({
-                key: "user_sid",
-                    secret: secretKey,
-                        store: sessionStore,
-                            resave: false,
-                                saveUninitialized: false,
-                                    cookie: {
-                                          maxAge: 24 * 60 * 60 * 1000, // 24 hours
-                                                httpOnly: true,
-                                                      secure: process.env.NODE_ENV === "production",
-                                                            sameSite: "lax",
-                                                                },
-                                                                  })
-                                                                  );
+console.log("Secret Key:", secretKey);
+// Session middleware configuration
+app.use(
+  session({
+    key: "user_sid",
+    secret: secretKey,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    },
+  })
+);
 
-                                                                  app.use(express.static(path.join(__dirname, "front-end")));
-                                                                  app.use(bodyParser.json());
-                                                                  app.use(
-                                                                    cors({
-                                                                        origin: "http://localhost:3001",
-                                                                            credentials: true,
-                                                                              })
-                                                                              );
+app.use(express.static(path.join(__dirname, "front-end")));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
 
-                                                                              // Middleware to log requests
-                                                                              app.use((req, res, next) => {
-                                                                                console.log(req.url);
-                                                                                  next();
-                                                                                  });
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
 
-                                                                                  // Serve the uploads directory
-                                                                                  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Serve the uploads directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-                                                                                  // Login route
-                                                                                  app.post("/", loginRoute);
+// Login route
+app.post("/", loginRoute);
 
-                                                                                  app.use("/notices", isLoggedIn, noticesRoute);
-                                                                                  app.use("/documents", isLoggedIn, documentsRoute);
-                                                                                  app.use("/users", isLoggedIn, usersRoute);
-                                                                                  app.use("/suppliers", isLoggedIn, suppliersRoute);
-                                                                                  app.use("/products", isLoggedIn, productsRoute);
-                                                                                  app.use("/orders", isLoggedIn, ordersRoute);
-                                                                                  app.use("/userProductsRequests", isLoggedIn, userProductsRequests);
-                                                                                  app.use("/getRequests", isLoggedIn, getRequests);
-                                                                                  app.use("/email", isLoggedIn, emailRoute);
-                                                                                  app.use("/constraints", isLoggedIn, constraintsRoute);
-                                                                                  app.use("/meetings", isLoggedIn, meetingsRoute);
-                                                                                  app.use("/customerMeetingsAdmin", isLoggedIn, customerMeetingAdminRoutes);
-                                                                                  app.use("/customerMeetingsUser", isLoggedIn, customerMeetingUserRoutes);
-                                                                                  app.use("/generalMeetings", isLoggedIn, generalMeetingRoute);
-                                                                                  app.use("/notification", isLoggedIn, notificationRoute);
+app.use("/notices", isLoggedIn, noticesRoute);
+app.use("/documents", isLoggedIn, documentsRoute);
+app.use("/users", isLoggedIn, usersRoute);
+app.use("/suppliers", isLoggedIn, suppliersRoute);
+app.use("/products", isLoggedIn, productsRoute);
+app.use("/orders", isLoggedIn, ordersRoute);
+app.use("/userProductsRequests", isLoggedIn, userProductsRequests);
+app.use("/getRequests", isLoggedIn, getRequests);
+app.use("/email", isLoggedIn, emailRoute);
+app.use("/constraints", isLoggedIn, constraintsRoute);
+app.use("/meetings", isLoggedIn, meetingsRoute);
+app.use("/customerMeetingsAdmin", isLoggedIn, customerMeetingAdminRoutes);
+app.use("/customerMeetingsUser", isLoggedIn, customerMeetingUserRoutes);
+app.use("/generalMeetings", isLoggedIn, generalMeetingRoute);
+app.use("/notification", isLoggedIn, notificationRoute);
 
-                                                                                  // Error handling for 404
-                                                                                  app.use((req, res, next) => {
-                                                                                    res.status(404).json({ message: "Route not found" });
-                                                                                    });
-
-                                                                                    app.listen(port, () => {
-                                                                                      console.log(`Server running on http://localhost:${port}`);
-                                                                                      });
+// Error handling for 404
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
